aa4313947e3257e6aca5329440dae80545898634
AlejandraVe <alejandra.vera.vero@gmail.com>
2024-08-05 13:34:23 +0200

    fixed typewriter in js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 function generateTongueTwister(event) {
   event.preventDefault();
- 
+
   new Typewriter("#tongue-twister", {
     strings: "hola hola caracola",
     autoStart: true,
     delay: 16,
-    cursor: "";
+    cursor: "",
   });
 }
 
