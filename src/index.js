0fd317c8d0ce4e0db3a0c465d8b4a5f16c889bbd
AlejandraVe <alejandra.vera.vero@gmail.com>
2024-08-05 13:32:24 +0200

    Added javascript to the tongue twister generator

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function generateTongueTwister(event) {
+  event.preventDefault();
+ 
+  new Typewriter("#tongue-twister", {
+    strings: "hola hola caracola",
+    autoStart: true,
+    delay: 16,
+    cursor: "";
+  });
+}
+
+let formElement = document.querySelector("#generator-form");
+formElement.addEventListener("submit", generateTongueTwister);
