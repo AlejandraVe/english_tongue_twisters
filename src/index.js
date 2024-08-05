e2d4945e9dba968497e6d4a7d82e3d8e6990a4be
AlejandraVe <alejandra.vera.vero@gmail.com>
2024-08-05 14:03:15 +0200

    Updated the style.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateTongueTwister(event) {
-  event.preventDefault();
-
+function displayTongueTwister(response) {
   new Typewriter("#tongue-twister", {
-    strings: "hola hola caracola",
+    strings: response.data.answer,
     autoStart: true,
     delay: 16,
     cursor: "",
   });
+}
+
+function generateTongueTwister(event) {
+  event.preventDefault();
+  let tongueTwisterInput = document.querySelector("#user-topic");
+  let apiKey = "131b90447daa3e3cfco5aa3tc6e2b482";
+  let prompt = `Tell me a tongue twister`;
+  let context = `Give a short and precise answer please. Just write the tongue twister.`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let waitParagraph = document.querySelector("#tongue-twister");
+  waitParagraph.innerHTML = "Generating a tongue twister for you...";
+  axios.get(apiURL).then(displayTongueTwister);
 }
 
 let formElement = document.querySelector("#generator-form");
