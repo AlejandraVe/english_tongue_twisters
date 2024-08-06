a6777fd15f5cf12762180f2dde740fb46c28b7bc
AlejandraVe <alejandra.vera.vero@gmail.com>
2024-08-06 11:47:19 +0200

    Changed the project to be about a Poem Generator. Also made it better for the user experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayTongueTwister(response) {
-  new Typewriter("#tongue-twister", {
+function displayPoem(response) {
+  new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     delay: 16,
@@ -7,18 +7,22 @@ function displayTongueTwister(response) {
   });
 }
 
-function generateTongueTwister(event) {
+function generatePoem(event) {
   event.preventDefault();
-  let tongueTwisterInput = document.querySelector("#user-topic");
+  
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "131b90447daa3e3cfco5aa3tc6e2b482";
-  let prompt = `Tell me a tongue twister`;
-  let context = `Give a short and precise answer please. Just write the tongue twister.`;
+  let prompt = `User instructions: Generate a short poem about ${instructionsInput.value}`;
+  let context = `You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem at the end with 'SheCodes AI' inside a <strong> element. Make suer you sign at the end and NOT at the beginning. Just write the poem.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem-element");
 
-  let waitParagraph = document.querySelector("#tongue-twister");
-  waitParagraph.innerHTML = "Generating a tongue twister for you...";
-  axios.get(apiURL).then(displayTongueTwister);
+ poemElement.classList.remove("hidden");
+
+  let waitParagraph = document.querySelector("#poem");
+  waitParagraph.innerHTML = `<div class="blink">⌛ Generating a poem for you... </div>`;
+  axios.get(apiURL).then(displayPoem);
 }
 
 let formElement = document.querySelector("#generator-form");
-formElement.addEventListener("submit", generateTongueTwister);
+formElement.addEventListener("submit", generatePoem);
